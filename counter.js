170258ac9e68ad1bb73c84c424642749d2cdc1ab
Nurlykhan Kopenov <nurlykhan.kopenov@gmail.com>
2023-06-15 01:30:07 +0900

    hello

--- counter.js
+++ counter.js
@@ -1,16 +1,17 @@
 export function setupCounter(element) {
   let counter = 0
   function notifyMe() {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js');
-    }
-    Notification.requestPermission(function(result) {
-    if (result === 'granted') {
-        navigator.serviceWorker.ready.then(function(registration) {
-        registration.showNotification('Hello');
-        });
-    }
-    });
+    new Notification('ello')
+    // if ('serviceWorker' in navigator) {
+    //     navigator.serviceWorker.register('sw.js');
+    // }
+    // Notification.requestPermission(function(result) {
+    // if (result === 'granted') {
+    //     navigator.serviceWorker.ready.then(function(registration) {
+    //     registration.showNotification('Hello');
+    //     });
+    // }
+    // });
   }
   element.addEventListener('click', () => notifyMe())
   
